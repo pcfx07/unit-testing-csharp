ddf7cdd28691a0b20866970bcd2a8788154bfa2e
lawly <m-plankl@hotmail.com>
2018-05-02 21:47:35 +0200

    inital commit

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    // TESSTRUNNER LOOKS FOR ALL CLASSES WITH THIS ATTRIBUTE
+    // RUN TEST: Ctrl+R A
+    [TestClass]
+    public class ReservationTests
+    {
+        // TESTRUNNER WILL RUN ALL METHODS WITH THIS ATTRIBUTE
+        [TestMethod]
+        // NAMING CONVENTION: MethodName_StateUnderTest_ExpectedBehavior
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // TRIPLE A CONVENTION: ARRANGE, ACT, ASSERT
+
+            // ARRANGE: INIT OBJECTS
+            var reservation = new Reservation();
+
+            // ACT: ACT ON THIS OBJECT
+            var result = reservation.CanBeCancelledBy(new User() { IsAdmin = true });
+
+            // ASSERT: VERIFY RESULT IS CORRECT
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsHimself_ReturnsTrue()
+        {
+            // ARRANGE
+            var reservation = new Reservation();
+            var user = new User() { IsAdmin = false };
+
+            // ACT
+            reservation.MadeBy = user;
+            var result = reservation.CanBeCancelledBy(user);
+
+            //ASSERT
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsSomeOneElse_ReturnsFalse()
+        {
+            // ARRANGE
+            var reservation = new Reservation();
+            var userA = new User();
+            var userB = new User();
+
+            // ACT
+            reservation.MadeBy = userA;
+            var result = reservation.CanBeCancelledBy(userB);
+
+            // ASSERT
+            Assert.IsFalse(result);
+        }
+    }
+}

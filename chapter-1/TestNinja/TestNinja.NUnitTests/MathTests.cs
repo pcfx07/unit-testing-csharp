7eee5757a750287ec2001f3acb402b71358227b8
lawly <m-plankl@hotmail.com>
2018-05-02 22:54:46 +0200

    unit test for math using rider

---
+++ chapter-1/TestNinja/TestNinja.NUnitTests/MathTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.NUnitTests
+{
+    // RIDER: RUN UNIT TEST, SELECT CLASS OR METHOD: Ctrl U, R
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+            
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentGreater_ReturnFirstArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+            
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+            
+            Assert.That(result, Is.EqualTo(2));            
+        }
+        
+        [Test]
+        public void Max_BothArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+            
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
